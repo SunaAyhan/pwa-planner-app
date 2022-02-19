63581a2d7427bbf69c65e92786683c47f4d15cf5
SunaAyhan <sunaayhan16@gmail.com>
2022-02-19 14:26:19 +0300

    "calendar" name is edited.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { HomePageComponent } from './HomePage/HomePage.component';
 import { NewCalenderComponent } from './NewCalender/NewCalender.component';
 
 const routes: Routes = [
-  {path:'mycalenders' , component:NewCalenderComponent },
-  {path:'callender/:id' , component:HomePageComponent},
+  {path:'mycalendars' , component:NewCalenderComponent },
+  {path:'calendar/:id' , component:HomePageComponent},
   {path:'home' , component:NewCalenderComponent}
 
 
